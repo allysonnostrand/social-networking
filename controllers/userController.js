44bdcc3689e81f2710512e14dde82f6789988641
iggy <allyson.nostrandd@gmail.com>
2022-05-23 16:26:28 -0700

    created user controller, routes index.js, and user routes

---
+++ controllers/userController.js
@@ -0,0 +1,86 @@
+const { User, Thought, Reaction } = require('../models');
+
+module.exports = {
+    // Get all users
+    getUsers(req, res) {
+      User.find()
+        .then(async (users) => {
+          const userObj = {
+            users
+          };
+          return res.json(userObj);
+        })
+        .catch((err) => {
+          console.log(err);
+          return res.status(500).json(err);
+        });
+    },
+    // Get a single user
+    getSingleUser(req, res) {
+      User.findOne({ _id: req.params.userId })
+        .select('-__v')
+        .lean()
+        .then(async (user) =>
+          !user
+            ? res.status(404).json({ message: 'No user with that ID' })
+            : res.json({
+                user,
+                thought: await Thought(req.params.userId),
+                friend: await User(req.params.userId),
+              })
+        )
+        .catch((err) => {
+          console.log(err);
+          return res.status(500).json(err);
+        });
+    },
+    // create a new user
+    createUser(req, res) {
+      User.create(req.body)
+        .then((user) => res.json(user))
+        .catch((err) => res.status(500).json(err));
+    },
+    // Delete a user and remove their thoughts
+    deleteUser(req, res) {
+      User.findOneAndRemove({ _id: req.params.userId })
+        .then((user) =>
+          !user
+            ? res.status(404).json({ message: 'No such user exists' })
+            : Thought.findOneAndUpdate(
+                { users: req.params.userId },
+                { $pull: { users: req.params.userId } },
+                { new: true }
+              )
+        )
+        .then((thought) =>
+          !thought
+            ? res.status(404).json({
+                message: 'User deleted, but no thoughts found',
+              })
+            : res.json({ message: 'User successfully deleted' })
+        )
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    },
+  
+    // update a user
+    updateUser(req, res) {
+      console.log('You are updating a user');
+      console.log(req.body);
+      User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { thoughts: req.body } },
+        { runValidators: true, new: true }
+      )
+        .then((user) =>
+          !user
+            ? res
+                .status(404)
+                .json({ message: 'No user found with that ID :(' })
+            : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+    }
+}
--- models/Thought.js
+++ models/Thought.js
@@ -18,6 +18,7 @@ const thoughtSchema = new Schema({
 {
     toJSON: {
         virtuals: true,
+        getters: true
     },
 
     id: false,
